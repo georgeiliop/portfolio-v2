1c730e1facafc060f2afb522999b7718768d67f2
georgeiliop <georgeiliopp@gmail.com>
2024-11-17 18:00:26 +0200

    small changes to portfolio project object

--- src/assets/projectList.js
+++ src/assets/projectList.js
@@ -14,7 +14,7 @@ export const projectList = {
     number: "01",
     title: "Portfolio V2",
     description:
-      "This is my second version of my portfolio. I plan on reworking it every year to represent me in the best way possible. I will probably keep an archive of past versions. As a project, I tried to reflect my personality and went through multiple designs.",
+      "This is the second version of my portfolio. I plan on reworking it every year to represent me in the best way possible. I will probably keep an archive of past versions. As a project, I tried to reflect my personality and went through multiple designs.",
     imageUrl: portfolioIntro,
     tags: ["React", "SASS", "UI", "Figma"],
     link: "https://github.com/georgeiliop/portfolio-v2",
@@ -22,10 +22,10 @@ export const projectList = {
       {
         subtitle: "Kit",
         subtitleDescription:
-          "With the design I focused on western and horror, things I love a lot. I used a creamy yellow for the main pages and a bold red for the extras.",
+          "With the design I focused on western and horror. Things I love a lot! Used a creamy yellow for the main pages and a bold red for the extras.",
         img: portfolioDesign,
         description:
-          "I ended up using 3 colors throughout, and different western fonts. The icons are also a western icons font pack. ",
+          "Ended up using 3 colors throughout, and different western fonts. The icons are also a western icons font pack. ",
       },
       {
         subtitle: "High Fidality Wireframes",
@@ -33,7 +33,7 @@ export const projectList = {
           "I worked on desktop and mobile wireframes to help me identify the end result I wanted to achieve and make the coding part faster.",
         img: portfolioDesignHifi,
         description:
-          "On my next iteration of my portfolio I will include low fi wireframes and will determine the flow beforehand to make it easier to design.",
+          "On my next iteration of the portfolio I will include low fi wireframes and will determine the flow beforehand to make it easier to design.",
       },
       {
         img: portfolioDesignHifiMobile,
@@ -45,32 +45,32 @@ export const projectList = {
       {
         subtitle: "Structure",
         subtitleDescription:
-          "Using React and SASS, I wanted to make minimal use of other frameworks and libraries. I used 3 main folders to structure the project. Assets, Components and Routes. I used the components folder for resuable components, and routes to include all the routes and the components that make up each route.",
+          "Using React and SASS, I wanted to make minimal use of other frameworks and libraries. I used 3 main folders to structure the project. Assets, Components and Routes. The components folder is for resuable components, and routes for all the pages and the components that make up each.",
         img: portfolioStructure,
         description:
-          "Each component has its own scss file, and I used a global file for colors and layout properties that I would use universally. For the projects I used a dynamic route approach",
+          "Each component has its own scss file, and I used a global file for colors and layout properties that I would use universally. Every project has a dynamic route approach",
       },
       {
         subtitle: "Header & Menu",
         subtitleDescription:
-          "For the header, I wanted to make it sticky when the user scroll up and to hide it while scrolling down. I used a listener for the scroll and a state to control the styles.",
+          "I wanted to make the header sticky when the user scroll up and to hide it while scrolling down. I used a listener for the scroll and a state to control the styles.",
         img: portfolioHeader,
         description:
-          "For the menu, I used media quaries to control when it shows up. When the menu is open, I disabled scolling. I also used animations for the burder menu button and for the color changes.",
+          "I used media quaries to control when the menu shows up. When its open, I disabled scolling. I also used animations for the burder menu button and for the color changes.",
       },
       {
         subtitle: "Motion",
         subtitleDescription:
-          "I used various animations throughout the page with different approaches. Listeners, hover transforms, keyframes and mount changes.",
+          "I made various animations throughout the page with different approaches. Listeners, hover transforms, keyframes and mount changes.",
         img: portfolioMotion,
       },
       {
         subtitle: "Responsive design",
         subtitleDescription:
-          "I used many media quaries throughout the project to achieve both desktop and mobile design. I also used dynamic font sizes, dynamic paddings and margins, and a lot of flexbox.",
+          "With media quaries throughout the project I implemented both the desktop and the mobile design. Also dynamic font sizes, dynamic paddings and margins, and a lot of flexbox.",
         img: portfolioResponsive,
         description:
-          "To create the slightly random grid on the extra page I used 2 grids and controlled the layout with the grid display.",
+          "To create the slightly random grid on the extra page I made 2 grids and controlled the layout with the grid display.",
       },
     ],
   },
